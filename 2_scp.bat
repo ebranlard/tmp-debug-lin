58395c66262edb3cc31a74d26455306169762a73
Emmanuel Branlard <emmanuel.branlard@nrel.gov>
2023-08-18 19:54:23 -0600

    Adding dev1 and 2

---
+++ 2_scp.bat
@@ -0,0 +1,13 @@
+
+
+
+scp -o MACs=hmac-sha2-512 ebranlar@eagle.hpc.nrel.gov:/projects/windse/pbortolo/outputs/temp-debug-lin/ua-dev0/*.lin  ua-dev0/*.lin
+scp -o MACs=hmac-sha2-512 ebranlar@eagle.hpc.nrel.gov:/projects/windse/pbortolo/outputs/temp-debug-lin/ua-dev1/*.lin  ua-dev1/*.lin
+scp -o MACs=hmac-sha2-512 ebranlar@eagle.hpc.nrel.gov:/projects/windse/pbortolo/outputs/temp-debug-lin/ua-dev2/*.lin  ua-dev2/*.lin
+
+scp -o MACs=hmac-sha2-512 ebranlar@eagle.hpc.nrel.gov:/projects/windse/pbortolo/outputs/temp-debug-lin/ua-dev0/*.outb  ua-dev0/*.outb
+scp -o MACs=hmac-sha2-512 ebranlar@eagle.hpc.nrel.gov:/projects/windse/pbortolo/outputs/temp-debug-lin/ua-dev1/*.outb  ua-dev1/*.outb
+scp -o MACs=hmac-sha2-512 ebranlar@eagle.hpc.nrel.gov:/projects/windse/pbortolo/outputs/temp-debug-lin/ua-dev2/*.outb  ua-dev2/*.outb
+
+
+
