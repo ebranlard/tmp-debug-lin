854936fb390e499be41bfc9ff96d49a69db9f622
Emmanuel Branlard <emmanuel.branlard@nrel.gov>
2023-08-18 20:00:08 -0600

    Getting only first lin file

--- 2_scp.bat
+++ 2_scp.bat
@@ -1,13 +1,13 @@
 
 
 
-scp -o MACs=hmac-sha2-512 ebranlar@eagle.hpc.nrel.gov:/projects/windse/pbortolo/outputs/temp-debug-lin/ua-dev0/*.lin  ua-dev0/*.lin
-scp -o MACs=hmac-sha2-512 ebranlar@eagle.hpc.nrel.gov:/projects/windse/pbortolo/outputs/temp-debug-lin/ua-dev1/*.lin  ua-dev1/*.lin
-scp -o MACs=hmac-sha2-512 ebranlar@eagle.hpc.nrel.gov:/projects/windse/pbortolo/outputs/temp-debug-lin/ua-dev2/*.lin  ua-dev2/*.lin
+scp -o MACs=hmac-sha2-512 ebranlar@eagle.hpc.nrel.gov:/projects/windse/pbortolo/outputs/temp-debug-lin/ua-dev0/*.1.lin  ua-dev0/
+scp -o MACs=hmac-sha2-512 ebranlar@eagle.hpc.nrel.gov:/projects/windse/pbortolo/outputs/temp-debug-lin/ua-dev1/*.1.lin  ua-dev1/
+scp -o MACs=hmac-sha2-512 ebranlar@eagle.hpc.nrel.gov:/projects/windse/pbortolo/outputs/temp-debug-lin/ua-dev2/*.1.lin  ua-dev2/
 
-scp -o MACs=hmac-sha2-512 ebranlar@eagle.hpc.nrel.gov:/projects/windse/pbortolo/outputs/temp-debug-lin/ua-dev0/*.outb  ua-dev0/*.outb
-scp -o MACs=hmac-sha2-512 ebranlar@eagle.hpc.nrel.gov:/projects/windse/pbortolo/outputs/temp-debug-lin/ua-dev1/*.outb  ua-dev1/*.outb
-scp -o MACs=hmac-sha2-512 ebranlar@eagle.hpc.nrel.gov:/projects/windse/pbortolo/outputs/temp-debug-lin/ua-dev2/*.outb  ua-dev2/*.outb
+scp -o MACs=hmac-sha2-512 ebranlar@eagle.hpc.nrel.gov:/projects/windse/pbortolo/outputs/temp-debug-lin/ua-dev0/*.outb  ua-dev0/
+scp -o MACs=hmac-sha2-512 ebranlar@eagle.hpc.nrel.gov:/projects/windse/pbortolo/outputs/temp-debug-lin/ua-dev1/*.outb  ua-dev1/
+scp -o MACs=hmac-sha2-512 ebranlar@eagle.hpc.nrel.gov:/projects/windse/pbortolo/outputs/temp-debug-lin/ua-dev2/*.outb  ua-dev2/
 
 
 
